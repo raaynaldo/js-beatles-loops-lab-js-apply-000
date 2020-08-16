650ef390eed4928fc56c187eb81f0821d02988a5
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-16 06:01:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   text = [];
+
   for (var i = 0; i < musicians.length; i++) {
     text[i] = `${musicians[i]} plays ${instruments[i]}`;
   }
+  
   return text;
 }
