c7170ee1d2dc185ce9ab34e58569b14d29ff40bb
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-16 06:01:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay(musicians, instruments){
   text = [];
   for (var i = 0; i < musicians.length; i++) {
-    text[i] = `${musicians[i]} plays  ${instruments[i]}`;
+    text[i] = `${musicians[i]} plays ${instruments[i]}`;
   }
 }
