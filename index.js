9a0762c9599dc33c12eed6d60b87769325e34efb
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-16 06:04:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var text = [];
-  console.log(musicians);
+
   for (var i = 0; i < musicians.length; i++) {
     text[i] = `${musicians[i]} plays ${instruments[i]}`;
   }
 
   return text;
+}
+
+function johnLennonFacts(facts){
+  var text = [];
+
+  for (var i = 0; i < facts.length; i++) {
+    text = facts[i] + !!!;
+  }
 }
